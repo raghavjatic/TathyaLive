5ae2f099e848e2886968010647973bb464fd0fa8
raghavjatic <raghavjatic@gmail.com>
2025-06-26 02:30:52 +0530

    Added Retrofit setup and integrated News API

---
+++ app/src/main/java/com/example/tathyalive/models/Source.kt
@@ -0,0 +1,6 @@
+package com.example.tathyalive.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/tathyalive/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.tathyalive.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "b8013169b84748ae9f566fc971b166eb"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
